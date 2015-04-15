5c82900c888766c434b2d65261b4a21983ec6625
Ernesto Anaya <ernestex@gmail.com>
2015-04-14 19:59:13 -0500

    distintos tipos de comentarios

---
+++ comentarios.php
@@ -0,0 +1,29 @@
+<?php
+
+// Estilo de comentario de usa sóla línea
+
+
+# otro estilo de una sóla línea
+
+
+/*
+Esto es un comentario
+multilinea.
+*/
+
+
+    /**
+     * Esto es un bloque docbook en php
+     * @param type $a
+     * @param type $b
+     * @return type
+     */
+    public function miFunc($a,$b) {
+        return $c;
+    }
+
+
+
+
+
+?>
