ae952a9a114ecca09e1b58379ce65d980e7f2467
Ernesto Anaya <ernestex@gmail.com>
2015-04-14 19:55:27 -0500

    ejemplo de etiquetas de apertura y cierre

---
+++ etiquetas.php
@@ -0,0 +1,18 @@
+
+<?php    // Modo mas convensional ?>
+
+
+
+<? // Usado generalemten en templates, necesita short_open_tag = On  ?>
+
+
+<?="imprime texto"?>
+
+
+<% // tags estilo ASP  %>
+
+
+<script language="PHP"> // estilo eqiqueta de HTML, muy raramente usado  </script>
+
+
+
